3cedf6087afb5beb216b2b4b37c815029a6a978c
Idris Haidir Ali <aliidir006@gmail.com>
2024-11-04 12:34:14 +0700

    Publish Code WEB

---
+++ script/main.js
@@ -0,0 +1,8 @@
+// Hamburger Menu Start
+const tombol = document.querySelector(".tombol");
+const menu = document.querySelector(".menu-navbar");
+
+tombol.addEventListener("click", () => {
+  menu.classList.toggle("aktif");
+});
+// Hamburger Menu END
