de11efb8ac4c3d8175c23447e44cb3e15df040a7
Idris Haidir Ali <aliidir006@gmail.com>
2024-11-07 21:55:35 +0700

    Header Carousel Telah dibuat Silahkan Buat Bagian Lain

--- script/main.js
+++ script/main.js
@@ -6,3 +6,21 @@ tombol.addEventListener("click", () => {
   menu.classList.toggle("aktif");
 });
 // Hamburger Menu END
+
+// Header STRART
+let currentIndex = 0;
+const slides = document.querySelectorAll(".carousel-slide");
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  const carousel = document.querySelector(".carousel");
+  carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 5000);
+// Header END
